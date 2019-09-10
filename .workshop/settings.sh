af4080975bf60be61ff5c7aad60c41bf48c3a638
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-09-10 12:57:57 +1000

    Add details of workshop for homeroom workshop index.

--- .workshop/settings.sh
+++ .workshop/settings.sh
@@ -1,3 +1,5 @@
 WORKSHOP_NAME=lab-building-images
+WORKSHOP_TITLE="Building Container Images"
+WORKSHOP_DESCRIPTION="A workshop on best practices for building container images"
 SPAWNER_PASSWORD=openshift
 WORKSHOP_MEMORY=2Gi

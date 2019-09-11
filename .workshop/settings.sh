d88b7e18721b87ae0034c4e9e059293c1dc4676a
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-09-11 19:47:09 +1000

    Default to using master image on quay.io.

--- .workshop/settings.sh
+++ .workshop/settings.sh
@@ -1,4 +1,5 @@
 WORKSHOP_NAME=lab-building-images
+WORKSHOP_IMAGE=quay.io/openshiftlabs/lab-building-images:master
 WORKSHOP_TITLE="Building Container Images"
 WORKSHOP_DESCRIPTION="A workshop on best practices for building container images"
 SPAWNER_PASSWORD=openshift

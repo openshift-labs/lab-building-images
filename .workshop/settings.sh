0577aa32f6c3086657aacd23750365af26f16420
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-10-07 19:32:14 +1100

    Tag version 1.8.

--- .workshop/settings.sh
+++ .workshop/settings.sh
@@ -1,5 +1,5 @@
 WORKSHOP_NAME=lab-building-images
-WORKSHOP_IMAGE=quay.io/openshiftlabs/lab-building-images:1.7
+WORKSHOP_IMAGE=quay.io/openshiftlabs/lab-building-images:1.8
 WORKSHOP_TITLE="Building Container Images"
 WORKSHOP_DESCRIPTION="A workshop on best practices for building container images"
 SPAWNER_PASSWORD=containers
